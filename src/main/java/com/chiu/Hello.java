3f8089896c69a5489c5544d7e197111f642a37f9
m10423010 <f5322583>
2019-01-22 23:00:19 +0800

    Java and kotlin && variable

--- src/main/java/com/chiu/Hello.java
+++ src/main/java/com/chiu/Hello.java
@@ -6,6 +6,19 @@ public class Hello {
         //new Person().Hello();
         Person p = new Person();
         p.Hello();
+
+        char a='A';// 只能儲存１個字元
+        Character b = 'c';
+        //float weight = 55.2; 在Java小數點一律使用double
+        float weight1 = 55.2f;//加上f就可以放在float裡了
+        Float x = Float.valueOf(25);
+        Double weight = 55.2;
+        String name = "邱俊鈐";
+        Integer age2 = 19;
+
+        System.out.println(b);
+
+
     }
 
 }
--- src/main/kotlin/com/chiu/Hello.kt
+++ src/main/kotlin/com/chiu/Hello.kt
@@ -3,7 +3,10 @@ package com.chiu
 fun main(args: Array<String>) {
     //Human().Hello()
     val h=Human()
+    val age =19 //也可以寫成 val age : int =19
     h.Hello()
+
+
 }
 
 //建立類別
